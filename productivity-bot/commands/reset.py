4f976c14ee467a9a1f736ce090dee9ad179f3eed
ElliottBarbeau <elliottbarbeau@gmail.com>
2025-08-11 15:16:29 -0400

    Revert temporary reset change

--- productivity-bot/commands/reset.py
+++ productivity-bot/commands/reset.py
@@ -11,6 +11,10 @@ class AdminCommands(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def reset_database(self, ctx):
         tables = [
+            "active_tasks_by_user",
+            "reminders_by_time",
+            "sessions_by_user_task",
+            "tasks_by_user",
             "daily_remaining_by_user"
         ]
 
